dbaed216050ce6f9bca1b167caea6a9e609fa0c7
Fernando Medina <fermmm@gmail.com>
2019-12-18 00:00:00 -0300

    Added root endpoint so aws don't complain

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,12 @@ const port = process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`Server running on ${port}!`));
 
+app.get('/', (req, res) => {
+   res.send({ message: 'Polyamory server' });
+});
+
 app.get('/hello/:value', (req, res) => {
-  console.log(`${req.method}: ${req.url}`);
-  const str = req.params.value as string;
-  res.send({ message: `Hello ${str}!` });
+   console.log(`${req.method}: ${req.url}`);
+   const str = req.params.value as string;
+   res.send({ message: `Hellooo ${str}!` });
 });
