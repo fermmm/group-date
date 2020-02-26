8475c23244f93e98644508fa4c561aebd684d721
Fernando Medina <fmedina@PX-ARG-EN-FM.local>
2020-02-26 01:42:37 -0300

    Fixed questions creation bug

--- src/components/user/questions/queries.ts
+++ src/components/user/questions/queries.ts
@@ -2,15 +2,18 @@ import * as gremlin from 'gremlin';
 import { g } from '../../../common-tools/database-tools/database-manager';
 import { GremlinResponse } from '../../../common-tools/database-tools/gremlin-typing-tools';
 
-
 export async function createQuestions(questionsId: number[]): Promise<GremlinResponse> {
-  let query2: gremlin.process.GraphTraversal | gremlin.process.GraphTraversalSource = g;
+   await g
+      .V()
+      .hasLabel('question')
+      .drop()
+      .iterate();
 
-  for (const question of questionsId) {
-    query2 = query2
-      .addV('question')
-      .property('questionId', question)
-  }
+   let query: gremlin.process.GraphTraversal | gremlin.process.GraphTraversalSource = g;
 
-  return (query2 as gremlin.process.GraphTraversal).next();
+   for (const question of questionsId) {
+      query = query.addV('question').property('questionId', question);
+   }
+
+   return (query as gremlin.process.GraphTraversal).next();
 }
