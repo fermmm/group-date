e9221c4285d70e2104b3a0372c81859ecff4b4e1
Fernando Medina <fermmm@gmail.com>
2020-04-24 02:01:57 -0300

    Implemented admin tests

--- src/components/testing/routes.ts
+++ src/components/testing/routes.ts
@@ -1,39 +1,14 @@
 import * as Router from '@koa/router';
 import { createFakeUsers } from '../../../test/tools/users';
 import { setTimeoutAsync } from '../../common-tools/js-tools/js-tools';
-import { ChatWithAdmins } from '../../shared-tools/endpoints-interfaces/admin';
-import { adminChatPost, allChatsWithAdminsGet, convertToAdmin } from '../admin/models';
 import { removeUsers } from '../common/queries';
 
-// TODO: Hacer los tests
+// TODO: Hay que trabajar en las notificaciones y despues en el aviso de nuevos usuarios
 export function testingRoutes(router: Router): void {
    router.get('/testing', async ctx => {
-      const fakeUsers = await createFakeUsers(3);
-      const mainUser = fakeUsers[0];
-      const mainUser2 = fakeUsers[1];
-      const adminUser = fakeUsers[2];
-
-      await convertToAdmin(adminUser.token);
-
-      await adminChatPost({ token: mainUser.token, messageText: 'hola que tal' }, null);
-      await adminChatPost({ token: mainUser.token, messageText: 'una pregunta' }, null);
-
-      await setTimeoutAsync(1500);
-
-      await adminChatPost({ token: mainUser2.token, messageText: 'holis' }, null);
-      await adminChatPost(
-         { token: adminUser.token, targetUserId: mainUser2.userId, messageText: 'hola que queres' },
-         null,
-      );
-
-      const chats: ChatWithAdmins[] = await allChatsWithAdminsGet(
-         { token: adminUser.token, excludeRespondedByAdmin: false },
-         null,
-      );
-
-      chats?.forEach(c => console.log(c.messages));
-
-      await removeUsers(fakeUsers);
+      // const fakeUsers = await createFakeUsers(4);
+      // const mainUser = fakeUsers[0];
+      // await removeUsers(fakeUsers);
       ctx.body = `Finished OK`;
    });
 }
---
+++ test/admin.test.ts
@@ -0,0 +1,94 @@
+import 'jest';
+import {
+   adminChatGet,
+   adminChatPost,
+   allChatsWithAdminsGet,
+   convertToAdmin,
+   convertToAdminPost,
+} from '../src/components/admin/models';
+import { retrieveFullyRegisteredUser } from '../src/components/common/models';
+import { removeUsers } from '../src/components/common/queries';
+import { ChatWithAdmins } from '../src/shared-tools/endpoints-interfaces/admin';
+import { User } from '../src/shared-tools/endpoints-interfaces/user';
+import { fakeCtx } from './tools/replacements';
+import { createFakeUsers } from './tools/users';
+
+describe('Users', () => {
+   let fakeUsers: User[];
+   let mainUser: User;
+   let mainUser2: User;
+   let adminUser: User;
+   let adminUserNatural: User;
+
+   beforeAll(async () => {
+      fakeUsers = await createFakeUsers(4);
+      mainUser = fakeUsers[0];
+      mainUser2 = fakeUsers[1];
+      adminUser = fakeUsers[2];
+      adminUserNatural = fakeUsers[3];
+      await convertToAdmin(adminUser.token);
+   });
+
+   test('Non admin users should not be able to convert other users into admins', async () => {
+      await convertToAdminPost({ token: mainUser.token, targetUserToken: adminUserNatural.token }, fakeCtx);
+      adminUserNatural = await retrieveFullyRegisteredUser(adminUserNatural.token, false, fakeCtx);
+      expect(adminUserNatural.isAdmin).toBeFalsy();
+   });
+
+   test('Admin users should be able to convert other users into admins', async () => {
+      await convertToAdminPost({ token: adminUser.token, targetUserToken: adminUserNatural.token }, fakeCtx);
+      adminUserNatural = await retrieveFullyRegisteredUser(adminUserNatural.token, false, fakeCtx);
+      expect(adminUserNatural.isAdmin).toBe(true);
+   });
+
+   test('Sending messages to admins works', async () => {
+      await adminChatPost({ token: mainUser.token, messageText: 'hola que tal' }, fakeCtx);
+      await adminChatPost({ token: mainUser.token, messageText: 'una pregunta' }, fakeCtx);
+
+      const chat: ChatWithAdmins = await adminChatGet({ token: mainUser.token }, fakeCtx);
+      expect(chat.messages.length).toBe(2);
+   });
+
+   test('Admins can read messages', async () => {
+      const chat: ChatWithAdmins = await adminChatGet(
+         { token: adminUserNatural.token, targetUserId: mainUser.userId },
+         fakeCtx,
+      );
+
+      expect(chat.messages.length).toBe(2);
+      expect(chat.nonAdminUser.userId).toBe(mainUser.userId);
+   });
+
+   test('Admins can send messages and identity of admins is hidden', async () => {
+      await adminChatPost({ token: mainUser2.token, messageText: 'holis' }, null);
+      await adminChatPost(
+         { token: adminUser.token, targetUserId: mainUser2.userId, messageText: 'hola que queres' },
+         null,
+      );
+
+      const chat: ChatWithAdmins = await adminChatGet({ token: mainUser2.token }, null);
+      expect(chat.messages.length).toBe(2);
+      expect(chat.messages[0].authorUserId).toBe(mainUser2.userId);
+      expect(chat.messages[1].authorUserId).toBeFalsy();
+   });
+
+   test('Admins can get a list of all chats', async () => {
+      const chats: ChatWithAdmins[] = await allChatsWithAdminsGet(
+         { token: adminUser.token, excludeRespondedByAdmin: false },
+         null,
+      );
+      expect(chats.length).toBe(2);
+   });
+
+   test('Admins can get a list of all chats filtered by responded by admins', async () => {
+      const chats: ChatWithAdmins[] = await allChatsWithAdminsGet(
+         { token: adminUser.token, excludeRespondedByAdmin: true },
+         null,
+      );
+      expect(chats.length).toBe(1);
+   });
+
+   afterAll(async () => {
+      await removeUsers(fakeUsers);
+   });
+});
