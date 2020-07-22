9d9c52bd4fba20f549ebc16d218c913277afa3d2
Fernando Medina <fermmm@gmail.com>
2020-07-22 04:01:14 -0300

    Imporved perfectly matched groups query

--- src/common-tools/database-tools/common-queries.ts
+++ src/common-tools/database-tools/common-queries.ts
@@ -4,12 +4,3 @@ import { Traversal } from './gremlin-typing-tools';
 export function valueMap(traversal: Traversal): Traversal {
    return traversal.valueMap().by(__.unfold());
 }
-
-export function gremlinArrayMap(list: Traversal, traversalForEachItem: Traversal): Traversal {
-   return list
-      .project('i')
-      .by(traversalForEachItem.fold())
-      .unfold()
-      .select(column.values)
-      .unfold();
-}
--- src/components/groups-finder/queries.ts
+++ src/components/groups-finder/queries.ts
@@ -1,15 +1,9 @@
-import { gremlinArrayMap } from '../../common-tools/database-tools/common-queries';
-import { __, column, g, order, P } from '../../common-tools/database-tools/database-manager';
+import { __, column, g, order, P, scope } from '../../common-tools/database-tools/database-manager';
 import { Traversal } from '../../common-tools/database-tools/gremlin-typing-tools';
 
 export function getAllPossibleGroups(): Traversal {
    let traversal = getUsersAllowedToBeOnNewGroups();
-   // TODO: Aca ordenar los matches no seria necesario por que los grupos se forman con muchos usuarios y luego se ordenan y luego se limitan
-   traversal = gremlinArrayMap(traversal, getMatchesOrderedByConnectionsAmount(__));
-   traversal = gremlinArrayMap(traversal, getMatchesSharedWithEachMatch(__));
-
-   // Remove duplicates and empty arrays from the list
-   traversal = traversal.dedup().where(__.unfold());
+   traversal = getMatchesSharedWithEachMatch(traversal);
    return traversal;
 }
 
@@ -17,14 +11,7 @@ export function getUsersAllowedToBeOnNewGroups(): Traversal {
    return g.V().hasLabel('user');
 }
 
-/**
- * Receives a traversal with a user and returns their "matches" (when they like each other).
- * The results are ordered by the amount of matches between them, the first users will be those who have
- * more matches with the other ones.
- *
- * @param traversal A traversal with a list of users
- */
-// TODO: Si un usuario no tiene amigos que devuelve esto, se ejecuta lo que venga despues de algo que devuelve vacio?
+/*
 export function getMatchesOrderedByConnectionsAmount(traversal: Traversal): Traversal {
    traversal = traversal
       .project('user', 'matches')
@@ -44,40 +31,44 @@ export function getMatchesOrderedByConnectionsAmount(traversal: Traversal): Trav
 
    return traversal;
 }
+*/
 
 /**
  * TODO: Que pasa si no devuelve nada en algun lado? no detendra toda la busqueda?
  */
 export function getMatchesSharedWithEachMatch(traversal: Traversal): Traversal {
-   // Save the user as "u" and then select the matches list
-   traversal = traversal
-      .as('t')
-      .select('user')
-      .as('appUser')
-      .select('t')
-      .select('matches')
-      .unfold();
+   return (
+      traversal
+         .project('userGroups')
+         .by(
+            __.as('appUser')
+               .both('Match')
+               .project('group')
+               .by(
+                  __.union(
+                     // Search the "matches in common" (matches of the match that are also matches of the user)
+                     __.both('Match')
+                        .where(P.neq('appUser'))
+                        .where(__.both('Match').where(P.eq('appUser'))),
 
-   let forEachMatch: Traversal = __.as('match');
+                     // The search above doesn't include the 2 users that are being checked against and they are also part of the group, so we add them in a union
+                     __.select('appUserMatch'),
+                     __.select('appUser'),
+                  )
+                     // We need to order here because dedup() removes duplicates if the order of the elements in the groups are the same
+                     .order()
+                     .by('userId') // TODO: aca hay que ordenar por la cantidad de conexiones que tienen entre si cada miembro del grupo por que despues se viene un limit(), o tambien se podria ordenar por que tanto necesitan estar en un grupo
+                     .values('name')
+                     .fold(),
+               )
+               .select(column.values)
+               .unfold(),
+         )
+         .select(column.values)
+         .unfold()
 
-   // Search the "matches in common" (matches of the match that are also matches of the user)
-   forEachMatch = forEachMatch
-      .both('Match')
-      .where(P.neq('appUser'))
-      .where(__.both('Match').where(P.eq('appUser')));
-
-   // If the matches in common has a minimum amount create a group adding the matches in common and the match
-   forEachMatch = forEachMatch.union(__.select('match'), __.select('appUser'), __.identity());
-
-   forEachMatch = forEachMatch
-      .unfold()
-      .order()
-      .by('userId'); // TODO: aca hay que ordenar por la cantidad de conexiones que tienen entre si cada miembro del grupo por que despues se viene un limit(), o tambien se podria ordenar por que tanto necesitan estar en un grupo
-
-   // For testing:
-   forEachMatch = forEachMatch.values('name');
-
-   return gremlinArrayMap(traversal, forEachMatch)
-      .dedup()
-      .fold();
+         // Remove empty arrays  and duplicates from the list
+         .where(__.count(scope.local).is(P.gt(2)))
+         .dedup()
+   );
 }
