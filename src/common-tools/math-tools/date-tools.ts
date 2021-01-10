07f30c4a39d96a4a03d8e7edbbfc4c71aa0c78af
Fernando Medina <fermmm@gmail.com>
2021-01-09 21:17:09 -0300

    Fixed import line

--- src/common-tools/math-tools/date-tools.ts
+++ src/common-tools/math-tools/date-tools.ts
@@ -1,4 +1,4 @@
-import moment = require("moment");
+import * as moment from "moment";
 import { YEAR_IN_SECONDS } from "./constants";
 
 export function fromBirthDateToAge(birthDate: number): number {
