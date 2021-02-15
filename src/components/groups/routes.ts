f14774025852060e5fbb57c24da9e063905f1ca7
Fernando Medina <fermmm@gmail.com>
2021-02-15 00:48:43 -0300

    Fixed chat get router bug

--- src/components/groups/routes.ts
+++ src/components/groups/routes.ts
@@ -13,7 +13,7 @@ import {
 export function groupsRoutes(router: Router): void {
    router.get("/group", async ctx => (ctx.body = await groupGet(ctx.request.query, ctx)));
    router.get("/user/groups", async ctx => (ctx.body = await userGroupsGet(ctx.request.query, ctx)));
-   router.get("/group/chat", async ctx => (ctx.body = await chatGet(ctx.request.body, ctx)));
+   router.get("/group/chat", async ctx => (ctx.body = await chatGet(ctx.request.query, ctx)));
    router.post("/group/accept", async ctx => (ctx.body = await acceptPost(ctx.request.body, ctx)));
    router.post("/group/ideas/vote", async ctx => (ctx.body = await dateIdeaVotePost(ctx.request.body, ctx)));
    router.post("/group/days/vote", async ctx => (ctx.body = await dateDayVotePost(ctx.request.body, ctx)));
