8d19230005d462662bec69b6ce00afcaef3a6681
Fernando Medina <fermmm@gmail.com>
2021-10-09 22:32:04 -0300

    Added basic testing function

--- src/components/testing/routes.ts
+++ src/components/testing/routes.ts
@@ -2,6 +2,8 @@ import * as Router from "@koa/router";
 import { BaseContext } from "koa";
 import { g, sendQuery, __ } from "../../common-tools/database-tools/database-manager";
 import { createRoute } from "../../common-tools/route-tools/route-tools";
+import { queryToGetAllUsers } from "../user/queries";
+import { fromQueryToUserList } from "../user/tools/data-conversion";
 import {
    createFakeChatConversation,
    createFakeTagsPost,
@@ -15,5 +17,7 @@ export function testingRoutes(r: Router): void {
    createRoute(r, "/testing/force-groups-search", "GET", forceGroupSearch);
    createRoute(r, "/testing/create-fake-chat", "GET", createFakeChatConversation);
 
-   createRoute(r, "/testing/temp", "GET", async (params: any, ctx: BaseContext) => {});
+   createRoute(r, "/testing/temp", "GET", async (params: any, ctx: BaseContext) => {
+      return await fromQueryToUserList(queryToGetAllUsers().limit(10));
+   });
 }
