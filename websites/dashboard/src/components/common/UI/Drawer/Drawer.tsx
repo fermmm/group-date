9adc54b93851fa9877bd6ddc0c57f842d6022393
Fernando Medina <fermmm@gmail.com>
2021-10-22 01:34:43 -0300

    Small changes to the dashboard drawer

--- websites/dashboard/src/components/common/UI/Drawer/Drawer.tsx
+++ websites/dashboard/src/components/common/UI/Drawer/Drawer.tsx
@@ -5,14 +5,17 @@ import {
    LogoContainer,
    ButtonsContainer,
    PageContainer,
-   ListItemStyled
+   ListItemStyled,
+   TooltipStyled
 } from "./styles.Drawer";
-import ListItem from "@mui/material/ListItem";
+import Zoom from "@mui/material/Zoom";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import ListItemText from "@mui/material/ListItemText";
 import logo from "../../../../assets/logo.png";
+import { IconButton, Tooltip } from "@mui/material";
 
 interface PropsDrawer {
+   expandOnHover?: boolean;
    buttons: Array<{
       icon: () => ReactNode;
       label: string;
@@ -22,21 +25,26 @@ interface PropsDrawer {
 }
 
 const Drawer: FC<PropsDrawer> = props => {
-   const { children, buttons } = props;
+   const { children, buttons, expandOnHover } = props;
    const [expanded, setExpanded] = useState(false);
 
    const handleDrawerExpand = () => {
-      setExpanded(true);
+      if (expandOnHover) {
+         setExpanded(true);
+      }
    };
 
    const handleDrawerContract = () => {
-      setExpanded(false);
+      if (expandOnHover) {
+         setExpanded(false);
+      }
    };
 
    return (
       <DrawerContainer>
          <ChildrenContainer
             expanded={expanded}
+            expandOnHover={expandOnHover}
             onMouseEnter={handleDrawerExpand}
             onMouseLeave={handleDrawerContract}
          >
@@ -47,20 +55,37 @@ const Drawer: FC<PropsDrawer> = props => {
                   alt={"logo"}
                />
             </LogoContainer>
-            <ButtonsContainer expanded={expanded}>
-               {buttons.map(button => (
-                  <ListItemStyled
-                     //@ts-ignore
-                     button={true}
-                     key={button.label}
-                     onClick={button.onClick}
-                     style={{ marginBottom: button.marginBottom ?? 0 }}
-                     sx={{ borderRadius: 2.2 }}
-                  >
-                     <ListItemIcon>{button.icon()}</ListItemIcon>
-                     <ListItemText primary={button.label} />
-                  </ListItemStyled>
-               ))}
+            <ButtonsContainer expanded={expanded} expandOnHover={expandOnHover}>
+               {buttons.map(button =>
+                  expandOnHover ? (
+                     <ListItemStyled
+                        //@ts-ignore
+                        button={true}
+                        key={button.label}
+                        onClick={button.onClick}
+                        style={{ marginBottom: button.marginBottom ?? 0 }}
+                        sx={{ borderRadius: 2.2 }}
+                     >
+                        <ListItemIcon>{button.icon()}</ListItemIcon>
+                        <ListItemText primary={button.label} />
+                     </ListItemStyled>
+                  ) : (
+                     <TooltipStyled
+                        title={button.label}
+                        arrow
+                        placement="right"
+                        TransitionComponent={Zoom}
+                        key={button.label}
+                     >
+                        <IconButton
+                           onClick={button.onClick}
+                           style={{ marginBottom: button.marginBottom ?? 0 }}
+                        >
+                           {button.icon()}
+                        </IconButton>
+                     </TooltipStyled>
+                  )
+               )}
             </ButtonsContainer>
          </ChildrenContainer>
          <PageContainer>{children}</PageContainer>
