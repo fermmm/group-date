2d4f5e95734a6fc0e686d158c50ed490f52bbc88
Fernando <fermmm@gmail.com>
2022-03-03 22:08:21 -0300

    Added App Store links

--- src/configurations.ts
+++ src/configurations.ts
@@ -21,7 +21,7 @@ import {
 export const APPLICATION_NAME = "GroupDate";
 export const APPLICATION_NAME_COMPLETE = "GroupDate - Non-monogamy group dating"; // Translated on usage
 export const GOOGLE_PLAY_URL = "https://play.google.com/store/apps/details?id=com.poly.dates";
-export const APP_STORE_URL = GOOGLE_PLAY_URL;
+export const APP_STORE_URL = "https://apps.apple.com/app/groupdate-polyamory-dating/id1610688573";
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////  CLIENT  /////////////////////////////////////////////////////
