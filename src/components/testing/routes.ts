94b9ca4ed4f5f157a88b1b241b75fae137d7c7c0
Fernando <fermmm@gmail.com>
2022-07-01 21:07:22 -0300

    Implemented migration for the new questions system

--- src/components/testing/routes.ts
+++ src/components/testing/routes.ts
@@ -3,16 +3,20 @@ import { BaseContext } from "koa";
 import { serializeIfNeeded } from "../../common-tools/database-tools/data-conversion-tools";
 import { cardinality, g, sendQuery, __ } from "../../common-tools/database-tools/database-manager";
 import { createRoute } from "../../common-tools/route-tools/route-tools";
+import { QUESTIONS } from "../../configurations";
+import { AnswerIds } from "../../shared-tools/endpoints-interfaces/user";
 import { encodeString } from "../../shared-tools/utility-functions/encodeString";
 import { GROUP_PROPS_TO_ENCODE, GROUP_PROPS_TO_ENCODE_AS_ARRAY } from "../../shared-tools/validators/group";
 import { TAG_PROPS_TO_ENCODE_AS_ARRAY } from "../../shared-tools/validators/tags";
 import { USER_PROPS_TO_ENCODE_AS_ARRAY } from "../../shared-tools/validators/user";
+import { fakeCtx } from "../../tests/tools/replacements";
 import { notifyUsersAboutNewCards } from "../cards-game/models";
 import { sendNewGroupNotification } from "../groups/models";
 import { queryToGetAllGroups, queryToGetAllGroupsOfUser, queryToGetGroupById } from "../groups/queries";
 import { fromQueryToGroupList } from "../groups/tools/data-conversion";
 import { queryToGetTags } from "../tags/queries";
 import { fromQueryToTagList } from "../tags/tools/data-conversion";
+import { userPost } from "../user/models";
 import { queryToGetAllUsers, queryToGetUserByToken } from "../user/queries";
 import { fromQueryToUser, fromQueryToUserList } from "../user/tools/data-conversion";
 import {
@@ -121,6 +125,66 @@ export function testingRoutes(r: Router): void {
       }
 
       console.log("Finished with tags");
+
+      /**
+       * NEW QUESTIONS MIGRATION
+       */
+      const allUsersWithTags = await fromQueryToUserList(
+         queryToGetAllUsers({ includeDemoAccounts: true }),
+         false,
+         true,
+      );
+
+      let usersDone = 0;
+      for (const user of allUsersWithTags) {
+         const questions: AnswerIds[] = [];
+
+         if (user.isCoupleProfile != null) {
+            questions.push({ questionId: "q05", answerId: user.isCoupleProfile ? "q05-a02" : "q05-a01" });
+         }
+
+         if (user.isCoupleProfile === true) {
+            questions.push({
+               questionId: "q06",
+               answerId: user.isUnicornHunter === true ? "q06-a01" : "q06-a02",
+            });
+         }
+
+         if (user.tagsSubscribed.find(t => t.tagId === "q03-a02-v2") != null) {
+            questions.push({ questionId: "taq-3-v2", answerId: "q03-a02-v2" });
+         }
+
+         if (user.tagsSubscribed.find(t => t.tagId === "q03-a01-v2") != null) {
+            questions.push({ questionId: "taq-3-v2", answerId: "q03-a01-v2" });
+         }
+
+         if (user.tagsSubscribed.find(t => t.tagId === "q03-a00-v2") != null) {
+            questions.push({ questionId: "taq-3-v2", answerId: "q03-a00-v2" });
+         }
+
+         if (user.tagsSubscribed.find(t => t.tagId === "q04-a01") != null) {
+            questions.push({ questionId: "taq-4", answerId: "q04-a01" });
+         }
+
+         if (user.tagsSubscribed.find(t => t.tagId === "q04-a02") != null) {
+            questions.push({ questionId: "taq-4", answerId: "q04-a02" });
+         }
+
+         if (user.tagsSubscribed.find(t => t.tagId === "q00-a00") != null) {
+            questions.push({ questionId: "taq-0", answerId: "q00-a00" });
+         }
+
+         if (user.tagsSubscribed.find(t => t.tagId === "q00-a01") != null) {
+            questions.push({ questionId: "taq-0", answerId: "q00-a01" });
+         }
+
+         await userPost(
+            { token: user.token, questionAnswers: questions, updateProfileCompletedProp: true },
+            fakeCtx,
+         );
+         usersDone++;
+         console.log(`done: ${usersDone}/${allUsersWithTags.length}`);
+      }
 
       // await notifyAllUsersAboutNewCards();
 
