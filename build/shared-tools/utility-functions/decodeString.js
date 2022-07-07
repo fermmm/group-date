526ee8a36fb53ff7e58b3c082273b5a454d77dd9
Fernando <fermmm@gmail.com>
2022-07-06 22:57:55 -0300

    Fixed potential crash with decodeUri()

--- build/shared-tools/utility-functions/decodeString.js
+++ build/shared-tools/utility-functions/decodeString.js
@@ -5,7 +5,13 @@ exports.decodeString = void 0;
  * Decodes a string using encodeString(), currently just calls decodeURI().
  */
 function decodeString(str) {
-    return decodeURI(str);
+    try {
+        return decodeURI(str);
+    }
+    catch (e) {
+        console.log("Warning: decodeUri() failed to decode te following string:", str);
+        return str;
+    }
 }
 exports.decodeString = decodeString;
 //# sourceMappingURL=decodeString.js.map
--- src/shared-tools/utility-functions/decodeString.ts
+++ src/shared-tools/utility-functions/decodeString.ts
@@ -2,5 +2,10 @@
  * Decodes a string using encodeString(), currently just calls decodeURI().
  */
 export function decodeString(str: string): string {
-   return decodeURI(str);
+   try {
+      return decodeURI(str);
+   } catch (e) {
+      console.log("Warning: decodeUri() failed to decode te following string:", str);
+      return str;
+   }
 }
